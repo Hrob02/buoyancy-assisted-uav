b129a4b506e02e115d63e664cb995f1ddf8aa479
Hallie Robins <hallie.robins@softwiresystems.com>
2026-03-27 11:20:07 +1100

    setting up gazebo

---
+++ ros_ws/src/uav_sim/launch/gazebo_vertical.launch.py
@@ -0,0 +1,32 @@
+import os
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, ExecuteProcess
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    uav_sim_dir = get_package_share_directory('uav_sim')
+    world_path = os.path.join(uav_sim_dir, 'worlds', 'empty_floor.world')
+    model_path = os.path.join(uav_sim_dir, 'models')
+    os.environ['GAZEBO_MODEL_PATH'] = model_path
+
+    return LaunchDescription([
+        ExecuteProcess(
+            cmd=['gazebo', '--verbose', world_path, '-s', 'libgazebo_ros_factory.so'],
+            output='screen'
+        ),
+        ExecuteProcess(
+            cmd=['ros2', 'run', 'gazebo_ros', 'spawn_entity.py',
+                 '-entity', 'simple_uav',
+                 '-file', os.path.join(model_path, 'simple_uav', 'model.sdf'),
+                 '-x', '0', '-y', '0', '-z', '1.0'],
+            output='screen'
+        ),
+        Node(
+            package='uav_sim',
+            executable='vertical_dynamics_node',
+            name='vertical_dynamics_node',
+            output='screen'
+        )
+    ])
